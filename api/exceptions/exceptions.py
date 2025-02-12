854fa751cfb3b0b9d105c2de3955c061582e64c8
timiwritescode <odebodezion@gmail.com>
2025-02-12 03:32:42 +0100

    fix: add correct status code to GET /api/v1/books/{book_id} endpoint

--- api/exceptions/exceptions.py
+++ api/exceptions/exceptions.py
@@ -22,6 +22,6 @@ class BookNotFoundException(AppBaseException):
     """
     def __init__(self, message="Book not found"):
         super().__init__(
-            status_code= status.HTTP_400_BAD_REQUEST,
+            status_code= status.HTTP_404_NOT_FOUND,
             message=message
         )
